465a25ff02a5ab38f15819aeef66c67a6f81a842
msaug <msaug@protonmail.com>
2021-10-29 09:29:21 +0200

    rarity calculation improvements

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,10 @@
+function ErrorMessage() {
+    return (
+      <div className="centeredTitle">
+        <h1 className="welcomeTitle">There was an error while fetching the data</h1>
+      </div>
+    );
+  }
+  
+  export default ErrorMessage;
+  
---
+++ src/components/InvalidAddress.js
@@ -0,0 +1,9 @@
+function InvalidAddress() {
+    return (
+      <div className="centeredTitle">
+        <h1 className="welcomeTitle">Invalid contract address</h1>
+      </div>
+    );
+  }
+  
+  export default InvalidAddress;
